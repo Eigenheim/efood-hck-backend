b1bb535e743d4d2fef9e781642a130333694af62
Niels Modry <uulan@student.kit.edu>
2017-12-09 10:05:14 +0100

    add bare class for TextPreProcessing

---
+++ src/main/java/github/eigenheim/efood/backend/components/textIndex/TextPreprocessor.java
@@ -0,0 +1,13 @@
+package github.eigenheim.efood.backend.components.textIndex;
+
+/**
+ * Preprocesses a String before it can be matched.
+ *
+ * @author nmodry
+ * @version 1.0
+ */
+public class TextPreprocessor {
+
+
+
+}
