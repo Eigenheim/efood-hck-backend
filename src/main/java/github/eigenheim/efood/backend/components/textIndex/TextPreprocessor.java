8dd2e8a7b686d26c8d988131627c90bf8b686791
Niels Modry <uulan@student.kit.edu>
2017-12-09 10:13:56 +0100

    add preprocessing method

--- src/main/java/github/eigenheim/efood/backend/components/textIndex/TextPreprocessor.java
+++ src/main/java/github/eigenheim/efood/backend/components/textIndex/TextPreprocessor.java
@@ -1,5 +1,9 @@
 package github.eigenheim.efood.backend.components.textIndex;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * Preprocesses a String before it can be matched.
  *
@@ -8,6 +12,30 @@ package github.eigenheim.efood.backend.components.textIndex;
  */
 public class TextPreprocessor {
 
+    /**
+     * Preprocesses a given String.
+     *
+     * Removes all non-alphabetic characters and splits into words.
+     *
+     * @param s the String that should be processed
+     * @return a list of preprocessed words
+     */
+    public static List<String> preprocess (String s) {
 
+        String[] lines = s.split("\n");
+        for (int i = 0; i < lines.length; i++) {
+            lines[i] = lines[i].replaceAll("[^a-zA-Z]", "");
+        }
+
+        final LinkedList<String> words = new LinkedList<>();
+
+        for (final String line : lines) {
+
+            words.addAll(Arrays.asList(line.split(" ")));
+
+        }
+
+        return words;
+    }
 
 }
